2e13ba3e11182d5b8ee670e202c151437692700b
ali-hammoud-dev <ali.hsn.hammoud@gmail.com>
2023-08-29 18:19:03 +0300

    user with server

--- app.py
+++ app.py
@@ -11,22 +11,22 @@ socketio = SocketIO(app)
 def index():
     return render_template('index.html')
 
-@socketio.on('message')
-def receive_message(message):
-    print(message)
-    send('this is a message from flask !!!!!!!')
-
-@socketio.on('custom event')
-def receive_custom_event(message):
-    print(f"the custom message is {message['name']}")
-    emit('from_flask','This is a custom event from flask.')
 
 
-@socketio.on('custom event click')
-def receive_custom_event(message):
-    print(f"the custom message is {message}")
-    emit('from flask',{'extension':'Flask-SocketIO'},json=True)
+# @socketio.on('message')
+# def receive_message(message):
+#     print(message)
+#     send('this is a message from flask !!!!!!!')
 
+# @socketio.on('custom event')
+# def receive_custom_event(message):
+#     print(f"the custom message is {message['name']}")
+#     emit('from_flask',{'extension':'This is a custom event from flask.'},json=True)
+
+@socketio.on('message from user')
+def receive_message_from_user(message):
+    print('USER MESSAGE: {}'.format(message))
+    emit('from flask',message)
 
 if __name__ == '__main__':
     socketio.run(app)
--- static/client.js
+++ static/client.js
@@ -1,21 +1,25 @@
 var socket = io.connect('http://127.0.0.1:5000');
 
-socket.on('connect', function() {
-    socket.send('I am now connected!');
+// socket.on('connect', function() {
+//     socket.send('I am now connected!');
 
-    socket.emit('custom event', {'name' : 'Anthony'});
+//     socket.emit('custom event', {'name' : 'Anthony'});
 
-    socket.on('from flask', function(msg) {
-        alert(msg['extension']);
-    });
+//     socket.on('from_flask', function(msg) {
+//         alert(msg['extension']);
+//     });
 
-    socket.on('message', function(msg) {
-        alert(msg);
-    });
-});
+//     socket.on('message', function(msg) {
+//         alert(msg);
+//     });
+// });
 
 function sendMessageToServer() {
     var message = document.getElementById('message').value;
-    socket.emit('custom event click',message);
+    socket.emit('message from user',message);
     document.getElementById('message').value = '';
 }
+
+socket.on('from flask',function(msg){
+    alert(msg)
+});
